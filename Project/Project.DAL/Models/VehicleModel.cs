aa798bd1062a58e770d4749dd2b7f828ae760fc6
tomislavjukic <t1jukic@etfos.hr>
2017-02-13 10:38:58 +0100

    nesto

---
+++ Project/Project.DAL/Models/VehicleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Models
+{
+    class VehicleModel
+    {
+        public Guid VehicleModelId { get; set; }
+        public Guid VehicleMakeId { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+
+        public virtual VehicleMake VehicleMake { get; set; }
+    }
+}
